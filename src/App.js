33c031e5635ff73070dadd1741f9d348a4dfa9c5
Jônatas Negri Grandini <30417446+jonaseu@users.noreply.github.com>
2020-06-15 21:39:55 -0300

    Click Button as function component (#1)
    Co-authored-by: Jonatas N Grandini <GRANDJ3@sa.ad.whirlpool.com>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import React from "react";
+import { useState } from "react"; //if module doesn't have an export dafault, it needs brackets
 import "./App.css";
+
+const myCLick = (setFiles, files) => {
+  const newFiles = { key1: "helloworld", ...files }; //spread operator,  it creates a copy of files, this way it doesn't point to the previous files,
+  setFiles(newFiles);
+};
+
+function ContainerUpload() {
+  const [files, setFiles] = useState({}); //{} is a dic | setFiles is the only function that control files var
+  console.log(files);
+  return (
+    <div>
+      <div>
+        <button
+          onClick={async () => {
+            //Check async
+            myCLick(setFiles, files);
+          }}
+        >
+          Click Me
+        </button>
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <ContainerUpload />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
