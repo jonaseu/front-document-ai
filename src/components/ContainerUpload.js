e209bc16d7bed4633898440dc12ce1b1dea50131
Jônatas Negri Grandini <30417446+jonaseu@users.noreply.github.com>
2020-07-06 21:22:38 -0300

    Working upload image and download processed file from backend (#2)
    * Upload working with HTTP axios post
    
    * Started connection with backend
    
    * meudeus nada funciona xico
    
    * working upload image and download processed file from backend

--- src/components/ContainerUpload.js
+++ src/components/ContainerUpload.js
@@ -1,39 +1,103 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import GCP_TOKEN from "./token.json";
+import socketIOClient from "socket.io-client";
 
-const ACCEPTED_FILE_EXTENSIONS = ["png", "jpg", "pdf"];
+const ACCEPTED_FILE_EXTENSIONS = { png: true, jpg: true, pdf: true };
+const BUCKET_NAME = "bucket-document-ai";
+const PROJECT_ID = "document-ai";
 
 const ContainerUpload = (props) => {
   const [files, setFiles] = useState([]);
+  const [filesProcessed, setFilesProcessed] = useState([]);
   const [disableBtn, setDisableBtn] = useState(true);
   const [validInput, setValidInput] = useState(true);
+  const [listening, setListening] = useState(false);
+
+  useEffect(() => {
+    let events;
+
+    const socket = socketIOClient("http://127.0.0.1:8080/", {
+      transports: ["websocket", "polling"],
+    });
+    socket.on("FileProcessing", (receivedData) => {
+      console.log(receivedData);
+      var blob = new Blob([receivedData.file[0]], {
+        type: "text/csv;charset=utf-8;",
+      });
+
+      const url = URL.createObjectURL(blob);
+      var link = document.createElement("a");
+      link.setAttribute("href", url);
+      link.setAttribute("download", `${receivedData.name}`);
+      link.click();
+    });
+  }, [filesProcessed]);
 
   const handleFileChange = (event) => {
     const receivedFiles = event.target.files;
     var areFilesValid = true;
 
-    //TBD: This shall probably go to a separate function, its objective is to check if all files inputed have the proper format
-    for (var i = 0; i < receivedFiles.length; i++) {
-      var receivedFileName = receivedFiles[i].name;
-      for (var extension in ACCEPTED_FILE_EXTENSIONS) {
-        if (
-          receivedFileName.toLowerCase().slice(-3) ==
-          ACCEPTED_FILE_EXTENSIONS[extension]
-        ) {
-          console.log("accepted");
-          areFilesValid = true;
-        }
+    const receivedFilesValid = Object.values(receivedFiles).map((file) => {
+      let validFiles = [];
+
+      const fileExtesion = file.name.toLowerCase().slice(-3);
+      if (ACCEPTED_FILE_EXTENSIONS[fileExtesion] === true) {
+        validFiles.push(file);
+      } else {
+        areFilesValid = false;
       }
-    }
+      return validFiles;
+    });
 
     if (areFilesValid) {
-      setFiles(receivedFiles);
+      setFiles(receivedFilesValid);
       setDisableBtn(false);
       setValidInput(true);
     } else {
+      setFiles([]);
+      setDisableBtn(true);
       setValidInput(false);
     }
+  };
 
-    console.log(receivedFiles.length);
+  const handleUploadFile = async () => {
+    let fd = new FormData();
+    const newDate = new Date(Date.now());
+    fd.name =
+      "fileInput_" +
+      newDate.toLocaleDateString().replace(/\//g, "-") +
+      "_" +
+      newDate.toLocaleTimeString();
+    files.map((f) => {
+      fd.append(f[0].name, f[0], f[0].name);
+    });
+
+    const gcpConfig = {
+      headers: {
+        Authorization: "Bearer " + GCP_TOKEN.token,
+      },
+    };
+    // const source = new EventSource("http://localhost:3000/respstadouploac");
+
+    // source.addEventListener();
+
+    const response = await axios.post("http://localhost:8080/upload", fd, {
+      headers: { "Content-Type": "multipart/form-data" },
+    });
+    // axios
+    //   .post(
+    //     // "https://storage.googleapis.com/upload/storage/v1/b/" +
+    //     //   BUCKET_NAME +
+    //     //   "/o?uploadType=media&name=" +
+    //     //   fd.name,
+    //     fd,
+    //     gcpConfig
+    //   )
+    //   .then((e) => console.log(e));
+
+    console.log("File Uploaded: " + fd.name);
+    console.log(response.data.body);
   };
 
   const invalidFileTypeWarning = () => {
@@ -46,7 +110,9 @@ const ContainerUpload = (props) => {
     <React.Fragment>
       <div>
         <div>
-          <button disabled={disableBtn}>Upload</button>
+          <button onClick={handleUploadFile} disabled={disableBtn}>
+            Upload
+          </button>
         </div>
         <div>
           <input type="file" onChange={handleFileChange} multiple />
